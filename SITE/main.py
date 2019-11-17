97595a3895e6163ca25642bf2d865b499c738b1a
noodle-lover <tooEthiopic@gmail.com>
2019-11-17 14:43:00 +0300

    bitly short links are found at bit.ly/x but their site is bitly.com so I there is no to include shortlinks in the site

--- SITE/main.py
+++ SITE/main.py
@@ -4,14 +4,14 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 # Bootstrap(app)
-
 app.config["SECRET_KEY"] = "tHISiSsECRET"
-@app.route("/",methods=["GET"])
-def index():
-    if request.args.get('url'):
-        url = request.args.get('url')
-        return redirect(url)
-    return redirect(url_for('home'))
+
+@app.route("/",methods=["GET","POST"])
+@app.route("/<slink>",methods=["GET","POST"])
+def index(slink=None):
+    if slink:
+        return render_template("404.html",slink=slink)
+    return redirect(url_for("home"))
 
 @app.route("/home",methods=["GET","POST"])
 def home():
