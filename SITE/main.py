85ff2187fa30b6f9c4a787e0a5e6cb7a03e4b56f
noodle-lover <tooEthiopic@gmail.com>
2019-11-19 15:36:26 +0300

    added some bootstrap css and flash messages

--- SITE/main.py
+++ SITE/main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import ExtendForm, LoginForm, RegisterForm
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "tHISiSsECRET"
 
 @app.route("/",methods=["GET","POST"])
-@app.route("/<slink>",methods=["GET","POST"])
+@app.route("/<slink>")
 def index(slink=None):
     if slink and slink[-1] is not '+':
         return render_template("index.html", title="Index", slink=slink)
@@ -16,16 +15,18 @@ def index(slink=None):
 def home():
     form = ExtendForm()
     if form.validate_on_submit():
-        url = form.url.data
+        url = form.url.data[::-1]
         return render_template("home.html", url=url, title="Home", form=form)    
     return render_template("home.html", title="Home", form=form)    
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"You have successfully logged in!! ",'success')
     return render_template("login.html", form=form)
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
     form = RegisterForm()
     return render_template("register.html", form=form)
