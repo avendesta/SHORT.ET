3213c99d6227a2efe495c1b3f2f668d481cf5dc5
noodle-lover <tooEthiopic@gmail.com>
2019-11-19 12:49:19 +0300

    using requirement.txt is better that virtualenv files

--- venv/bin/vba_extract.py
+++
@@ -1,4 +0,0 @@
-#!/home/duck/Desktop/SHORT.ET/venv/bin/python3
-# EASY-INSTALL-SCRIPT: 'XlsxWriter==1.2.6','vba_extract.py'
-__requires__ = 'XlsxWriter==1.2.6'
-__import__('pkg_resources').run_script('XlsxWriter==1.2.6', 'vba_extract.py')
