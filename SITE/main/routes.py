6763cceb57113f3e72e784ed1d8c42bfc5c2e13e
Rick Sanchez <noodle-lover>
2019-11-25 19:01:55 +0300

    Added jinja for nevbar so that its display changes based on loggedin or not, also added few more sample routes

--- SITE/main/routes.py
+++ SITE/main/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, request
 from main import app, db, bcrypt
 from main.models import User, Link, Click
 from main.forms import ExtendForm, LoginForm, RegisterForm
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -24,18 +24,25 @@ def home():
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f"Incorrect Email or Password!! ",'danger')
     return render_template("login.html", title="login", form=form)
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -46,6 +53,26 @@ def register():
         return redirect(url_for('login'))    
     return render_template("register.html", title="register", form=form)
 
+@app.route('/about')
+def about():
+    return "This is about page"
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have logged out successfully",'info')
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return f"Logged in as {current_user.username}"
+
+@app.route('/links')
+@login_required
+def links():
+    return "MY LINKS GOES HERE"
 
 @app.errorhandler(404)
 def not_found(e):
