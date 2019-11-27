0575f1b439d035780a89a26078b011fa2afa1c74
Rick Sanchez <noodle-lover>
2019-11-27 19:30:43 +0300

    Added a profile picture to account route/page

--- SITE/short/routes.py
+++ SITE/short/routes.py
@@ -80,7 +80,8 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
-    return render_template('account.html',title='Account',current_user=current_user)
+    image_file = url_for('static',filename='profile_pic/'+ current_user.image_file)
+    return render_template('account.html',title='Account',current_user=current_user, image_file=image_file)
 
 
 @app.errorhandler(404)
